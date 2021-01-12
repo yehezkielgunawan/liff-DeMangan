45bedf048bbf0256ce9645a6efc815308f54dde4
Yehezkiel Gunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-01-12 18:07:38 +0700

    footer edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Link, Text } from "@chakra-ui/react";
 import "../helper/helper";
 import { getCurrentYear } from "../helper/helper";
 
 export const Footer = () => (
-  <Flex as="footer" justifyContent="center" py="4rem">
+  <Flex as="footer" justifyContent="center" py="4rem" flexDirection="column" textAlign="center">
     <Text>
       {getCurrentYear()} | <b>Yehezkiel Gunawan</b>
     </Text>
+    <Link href="https://www.freepik.com/vectors/food">Food vector created by catalyststuff - www.freepik.com</Link>
+    <Link href="https://sznm.dev/">Design Inspiration from Agustinus Nathaniel - sznm.dev</Link>
   </Flex>
 );
